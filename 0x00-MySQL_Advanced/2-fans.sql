5c968dc61ec2fd5d1fefe18984b46a19611f9200
AyemDanm <danielMMomodu@gmail.com>
2024-11-24 23:58:24 +0100

    change2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Task: Rank country origins of metal bands, ordered by the number of non-unique fans
 SELECT origin, 
-       SUM(fans) AS total_fans
+       SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin               -- Group by country of origin
-ORDER BY total_fans DESC;     -- Order by the total number of fans, in descending order
+ORDER BY nb_fans DESC;     -- Order by the total number of fans, in descending order
