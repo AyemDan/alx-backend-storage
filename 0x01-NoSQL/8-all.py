cfb8879d218d7a97a3172d0b4eb212036505250c
AyemDanm <danielMMomodu@gmail.com>
2024-12-07 16:03:27 +0100

    change

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-""" 
-This module provides a function to list all documents
-in a MongoDB collection.
+"""
+This module provides a function to list all documents in a MongoDB collection.
+
 Functions:
-list_all(mongo_collection: Collection) -> list: 
+    list_all(mongo_collection: Collection) -> list:
+        Lists all documents in the specified MongoDB collection.
 """
 
 from pymongo.collection import Collection
 
-
 def list_all(mongo_collection: Collection):
     """
     Lists all documents in a MongoDB collection.
-
-    :param mongo_collection: pymongo collection object
-    :return: List of documents in the collection, or an empty list if none
+    
+    :param mongo_collection: A pymongo collection object representing the collection 
+                              from which to retrieve documents.
+    :type mongo_collection: Collection
+    :return: A list of documents in the collection, or an empty list if none are found.
+    :rtype: list
+    :raises ValueError: If the provided mongo_collection is None.
+    
+    Example:
+        collection = db.my_collection
+        documents = list_all(collection)
     """
     if mongo_collection is None:
         return []
